8daf5461a54c7aa3d151dc5f1ec8ba90df81daf6
Puttekungen <Victor.nordvall@gmail.com>
2024-11-08 09:55:52 +0100

    började lite

--- zelda.py
+++ zelda.py
@@ -1,4 +1,15 @@
-print("Hello World")
-print("HeLlO!")
-print("Testing")
-print("HEJSAN SVEJSAN")
+lista = []
+
+import random
+import sys
+
+print("1.Start game")
+print("2.Exit")
+meny = input("")
+if meny == "2":
+    print("Nu avslutas programmet...")
+    sys.exit()
+elif meny == "1":
+    print("heheh")   
+
+
