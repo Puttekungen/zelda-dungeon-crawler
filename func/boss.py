afe2387836fd9824099cdf4f91af2be0f822bf95
Puttekungen <Victor.nordvall@gmail.com>
2024-11-22 09:55:35 +0100

    La till +1 lvl i chest och combat

---
+++ func/boss.py
@@ -0,0 +1,13 @@
+import random
+
+def boss():
+    which = random.randint(1,3)
+
+    if which == 1:
+        rat_king
+
+    elif which == 2:
+        Giant Skeleton
+
+    elif which == 3:
+        God
